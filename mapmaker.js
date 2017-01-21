5c5104fca7bbf57a398ba6f8aa64c4a5c3e180e5
Jeff Zambory <zambory@live.unc.edu>
2017-01-21 14:17:18 -0500

    mapMaker.js

---
+++ mapmaker.js
@@ -0,0 +1,296 @@
+	var data = [{
+      "id":"1485022282-5883a44ad3279",
+      "key":"1485022282-5883a44ad3279",
+      "value":{
+         "rev":"1-72be56c06793cfe244d2cc26680e2b16"
+      },
+      "doc":{
+         "_id":"1485022282-5883a44ad3279",
+         "_rev":"1-72be56c06793cfe244d2cc26680e2b16",
+         "dateTimePosted":1485022282,
+         "severity":3,
+         "email":"1485022282@gmail.com",
+         "name":"Josh",
+         "phone":1485022282,
+         "lat":"33.912943",
+         "lng":"-84.454191",
+         "picture":"",
+         "address":"715 Peachtree Street Northeast, Atlanta, GA",
+         "description":"Dead tree",
+         "category":"Trees \/ Dead, Dying, Hazardous Trees",
+         "image":"http:\/\/3.bp.blogspot.com\/-bMf_r22jThI\/UAb6XroA8OI\/AAAAAAAAAP8\/7UgGXR0_M9A\/s1600\/10-sheep-tree.jpg"
+      }
+   },
+   {
+      "id":"1485022283-5883a44b0b4e9",
+      "key":"1485022283-5883a44b0b4e9",
+      "value":{
+         "rev":"1-3904ebeb88db2f1eb33b1ce55a4bb18a"
+      },
+      "doc":{
+         "_id":"1485022283-5883a44b0b4e9",
+         "_rev":"1-3904ebeb88db2f1eb33b1ce55a4bb18a",
+         "dateTimePosted":1485022283,
+         "severity":7,
+         "email":"1485022283@gmail.com",
+         "name":"Josh",
+         "phone":1485022283,
+         "lat":"33.419927",
+         "lng":"-84.694997",
+         "picture":"",
+         "address":"715 Peachtree Street Northeast, Atlanta, GA",
+         "description":"Dead tree",
+         "category":"Trees \/ Dead, Dying, Hazardous Trees",
+         "image":"http:\/\/3.bp.blogspot.com\/-bMf_r22jThI\/UAb6XroA8OI\/AAAAAAAAAP8\/7UgGXR0_M9A\/s1600\/10-sheep-tree.jpg"
+      }
+   },
+   {
+      "id":"1485022283-5883a44b3b822",
+      "key":"1485022283-5883a44b3b822",
+      "value":{
+         "rev":"1-35dd1de6484ff894935c5651fb5b8159"
+      },
+      "doc":{
+         "_id":"1485022283-5883a44b3b822",
+         "_rev":"1-35dd1de6484ff894935c5651fb5b8159",
+         "dateTimePosted":1485022283,
+         "severity":7,
+         "email":"1485022283@gmail.com",
+         "name":"Josh",
+         "phone":1485022283,
+         "lat":"33.531425",
+         "lng":"-84.230541",
+         "picture":"",
+         "address":"715 Peachtree Street Northeast, Atlanta, GA",
+         "description":"Dead tree",
+         "category":"Trees \/ Dead, Dying, Hazardous Trees",
+         "image":"http:\/\/3.bp.blogspot.com\/-bMf_r22jThI\/UAb6XroA8OI\/AAAAAAAAAP8\/7UgGXR0_M9A\/s1600\/10-sheep-tree.jpg"
+      }
+   },
+   {
+      "id":"1485022283-5883a44b6adeb",
+      "key":"1485022283-5883a44b6adeb",
+      "value":{
+         "rev":"1-923164618ab62e7ddb2bb921e6efdc01"
+      },
+      "doc":{
+         "_id":"1485022283-5883a44b6adeb",
+         "_rev":"1-923164618ab62e7ddb2bb921e6efdc01",
+         "dateTimePosted":1485022283,
+         "severity":1,
+         "email":"1485022283@gmail.com",
+         "name":"Josh",
+         "phone":1485022283,
+         "lat":"33.643823",
+         "lng":"-84.817095",
+         "picture":"",
+         "address":"715 Peachtree Street Northeast, Atlanta, GA",
+         "description":"Dead tree",
+         "category":"Trees \/ Dead, Dying, Hazardous Trees",
+         "image":"http:\/\/3.bp.blogspot.com\/-bMf_r22jThI\/UAb6XroA8OI\/AAAAAAAAAP8\/7UgGXR0_M9A\/s1600\/10-sheep-tree.jpg"
+      }
+   },
+   {
+      "id":"1485022283-5883a44b9b40a",
+      "key":"1485022283-5883a44b9b40a",
+      "value":{
+         "rev":"1-9f496ee9c002b2957763447c206af042"
+      },
+      "doc":{
+         "_id":"1485022283-5883a44b9b40a",
+         "_rev":"1-9f496ee9c002b2957763447c206af042",
+         "dateTimePosted":1485022283,
+         "severity":3,
+         "email":"1485022283@gmail.com",
+         "name":"Josh",
+         "phone":1485022283,
+         "lat":"33.477313",
+         "lng":"-84.801781",
+         "picture":"",
+         "address":"715 Peachtree Street Northeast, Atlanta, GA",
+         "description":"Dead tree",
+         "category":"Trees \/ Dead, Dying, Hazardous Trees",
+         "image":"http:\/\/3.bp.blogspot.com\/-bMf_r22jThI\/UAb6XroA8OI\/AAAAAAAAAP8\/7UgGXR0_M9A\/s1600\/10-sheep-tree.jpg"
+      }
+   },
+   {
+      "id":"1485022283-5883a44bcc604",
+      "key":"1485022283-5883a44bcc604",
+      "value":{
+         "rev":"1-708eb0b5661a2885a1d8d9fe5a6648d3"
+      },
+      "doc":{
+         "_id":"1485022283-5883a44bcc604",
+         "_rev":"1-708eb0b5661a2885a1d8d9fe5a6648d3",
+         "dateTimePosted":1485022283,
+         "severity":9,
+         "email":"1485022283@gmail.com",
+         "name":"Josh",
+         "phone":1485022283,
+         "lat":"33.655428",
+         "lng":"-84.655185",
+         "picture":"",
+         "address":"715 Peachtree Street Northeast, Atlanta, GA",
+         "description":"Dead tree",
+         "category":"Trees \/ Dead, Dying, Hazardous Trees",
+         "image":"http:\/\/3.bp.blogspot.com\/-bMf_r22jThI\/UAb6XroA8OI\/AAAAAAAAAP8\/7UgGXR0_M9A\/s1600\/10-sheep-tree.jpg"
+      }
+   },
+   {
+      "id":"1485022284-5883a44c085d2",
+      "key":"1485022284-5883a44c085d2",
+      "value":{
+         "rev":"1-9c3be3271e19146ed6c975114780cc0d"
+      },
+      "doc":{
+         "_id":"1485022284-5883a44c085d2",
+         "_rev":"1-9c3be3271e19146ed6c975114780cc0d",
+         "dateTimePosted":1485022284,
+         "severity":5,
+         "email":"1485022284@gmail.com",
+         "name":"Josh",
+         "phone":1485022284,
+         "lat":"33.662292",
+         "lng":"-84.703710",
+         "picture":"",
+         "address":"715 Peachtree Street Northeast, Atlanta, GA",
+         "description":"Dead tree",
+         "category":"Trees \/ Dead, Dying, Hazardous Trees",
+         "image":"http:\/\/3.bp.blogspot.com\/-bMf_r22jThI\/UAb6XroA8OI\/AAAAAAAAAP8\/7UgGXR0_M9A\/s1600\/10-sheep-tree.jpg"
+      }
+   },
+   {
+      "id":"1485022284-5883a44c32ec8",
+      "key":"1485022284-5883a44c32ec8",
+      "value":{
+         "rev":"1-059b4cd3aa368b6879128d6dcbeaae53"
+      },
+      "doc":{
+         "_id":"1485022284-5883a44c32ec8",
+         "_rev":"1-059b4cd3aa368b6879128d6dcbeaae53",
+         "dateTimePosted":1485022284,
+         "severity":3,
+         "email":"1485022284@gmail.com",
+         "name":"Josh",
+         "phone":1485022284,
+         "lat":"33.457308",
+         "lng":"-84.869112",
+         "picture":"",
+         "address":"715 Peachtree Street Northeast, Atlanta, GA",
+         "description":"Dead tree",
+         "category":"Trees \/ Dead, Dying, Hazardous Trees",
+         "image":"http:\/\/3.bp.blogspot.com\/-bMf_r22jThI\/UAb6XroA8OI\/AAAAAAAAAP8\/7UgGXR0_M9A\/s1600\/10-sheep-tree.jpg"
+      }
+   },
+   {
+      "id":"1485022284-5883a44c5a0a0",
+      "key":"1485022284-5883a44c5a0a0",
+      "value":{
+         "rev":"1-14e9d3cae260d57179fd9aef20832b85"
+      },
+      "doc":{
+         "_id":"1485022284-5883a44c5a0a0",
+         "_rev":"1-14e9d3cae260d57179fd9aef20832b85",
+         "dateTimePosted":1485022284,
+         "severity":4,
+         "email":"1485022284@gmail.com",
+         "name":"Josh",
+         "phone":1485022284,
+         "lat":"33.115703",
+         "lng":"-84.358972",
+         "picture":"",
+         "address":"715 Peachtree Street Northeast, Atlanta, GA",
+         "description":"Dead tree",
+         "category":"Trees \/ Dead, Dying, Hazardous Trees",
+         "image":"http:\/\/3.bp.blogspot.com\/-bMf_r22jThI\/UAb6XroA8OI\/AAAAAAAAAP8\/7UgGXR0_M9A\/s1600\/10-sheep-tree.jpg"
+      }
+   },
+   {
+      "id":"1485022284-5883a44c81004",
+      "key":"1485022284-5883a44c81004",
+      "value":{
+         "rev":"1-54e72cc8956d3211e5f8c401acf012bd"
+      },
+      "doc":{
+         "_id":"1485022284-5883a44c81004",
+         "_rev":"1-54e72cc8956d3211e5f8c401acf012bd",
+         "dateTimePosted":1485022284,
+         "severity":5,
+         "email":"1485022284@gmail.com",
+         "name":"Josh",
+         "phone":1485022284,
+         "lat":"33.466481",
+         "lng":"-84.588387",
+         "picture":"",
+         "address":"715 Peachtree Street Northeast, Atlanta, GA",
+         "description":"Dead tree",
+         "category":"Trees \/ Dead, Dying, Hazardous Trees",
+         "image":"http:\/\/3.bp.blogspot.com\/-bMf_r22jThI\/UAb6XroA8OI\/AAAAAAAAAP8\/7UgGXR0_M9A\/s1600\/10-sheep-tree.jpg"
+      }
+   },
+   {
+      "id":"1485022284-5883a44cae6ee",
+      "key":"1485022284-5883a44cae6ee",
+      "value":{
+         "rev":"1-3dd4ebd2ee2e66546b1b753381a67d53"
+      },
+      "doc":{
+         "_id":"1485022284-5883a44cae6ee",
+         "_rev":"1-3dd4ebd2ee2e66546b1b753381a67d53",
+         "dateTimePosted":1485022284,
+         "severity":5,
+         "email":"1485022284@gmail.com",
+         "name":"Josh",
+         "phone":1485022284,
+         "lat":"33.158259",
+         "lng":"-84.389214",
+         "picture":"",
+         "address":"715 Peachtree Street Northeast, Atlanta, GA",
+         "description":"Dead tree",
+         "category":"Trees \/ Dead, Dying, Hazardous Trees",
+         "image":"http:\/\/3.bp.blogspot.com\/-bMf_r22jThI\/UAb6XroA8OI\/AAAAAAAAAP8\/7UgGXR0_M9A\/s1600\/10-sheep-tree.jpg"
+      }
+   }];
+	
+	function initMap() {
+		var myLatLng = {lat:33.776531, lng:-84.413242};
+		var geocoder;
+		var map;
+		var contentString;
+		var arry=[];
+		map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 11,
+          center: myLatLng
+        });
+		
+		
+		console.log(data[0]);
+		console.log(data[1]);
+	}
+	function geocodeAddress(geocoder, resultsMap) {
+ 
+       
+			
+				var currentString = '<h3>' +"Address: " + '</h3>' +address + '<h3>' +"Description: " + '</h3>' + description + '<h3>' +"Pick Up Time: " + '</h3>' + PickUp +  '<h3>' +"Click below to pick up the food from the resturant: " + '</h3>' +'<input type="button" value= "Pick Up Food" id="PickUpFood" onclick="markerColorChange(' + uniquePickupId + ')">' +  '<h3>' +"Click below if the food has been picked up: " + '<input type="button" value= "Delete" id="DeleteMarker" onclick="deleteMarker(' + uniquePickupId + ')">';
+				resultsMap.setCenter(results[0].geometry.location);
+				var infowindow = new google.maps.InfoWindow({
+					//has to be the current String
+				  content: currentString
+				});
+				
+				var marker = new google.maps.Marker({
+				  map: resultsMap,
+				  position: results[0].geometry.location,
+				  icon: 'http://maps.google.com/mapfiles/ms/icons/red-dot.png'
+				});
+				markerList[uniquePickupId] = marker;
+				//onclick set content
+				var markerClick = function() {
+					infowindow.setContent(currentString);
+					infowindow.open(map, marker);	
+				};
+				marker.addListener('click', markerClick);
+				uniquePickupId ++;
+			
+      
+      }	
